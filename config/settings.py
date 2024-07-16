77e6833041e4bd26e9d5cf6c7a24287a0714e788
Samiksha <swabhimankhadka2001@gmail.com>
2024-07-16 10:32:25 +0545

    redis

--- config/settings.py
+++ config/settings.py
@@ -178,6 +178,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
